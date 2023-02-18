0e6bfaa9bd3d76dac3725cb1975b98cc2a047836
Wil Dias <wildias17@gmail.com>
2023-02-17 23:49:47 -0300

    Add files via upload

---
+++ ControleEmprestimo/ControleEmprestimo.WEB/Controllers/PessoaController.cs
@@ -0,0 +1,16 @@
+﻿using ControleEmprestimo.DATA.Models;
+using ControleEmprestimo.DATA.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleEmprestimo.WEB.Controllers
+{
+    public class PessoaController : Controller
+    {
+        private PessoaService oPessoaService = new PessoaService();
+        public IActionResult Index()
+        {
+            List<Pessoa> oListPessoa = oPessoaService.oRepositoryPessoa.SelecionarTodos();
+            return View(oListPessoa);
+        }
+    }
+}
